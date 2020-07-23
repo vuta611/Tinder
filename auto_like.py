b81b3abe7d0432cf1db7444003815f2dfe7fef2b
vuta <vuta@galaxyone.com.vn>
2020-07-23 11:31:23 +0700

    Improve code flow

--- auto_like.py
+++ auto_like.py
@@ -2,19 +2,9 @@ import tinder_api_sms
 import features
 import random
 
-locations = {
-    '10.766869':'106.692811', 
-    '21.035127':'105.851987', 
-    '16.065015':'108.221882', 
-    '11.947322':'108.440285',
-    '10.782762':'106.696145',
-    '10.033279':'105.788268',
-    '20.855117': '106.682231'
-}
-
 places = {
     ('10.766869', '106.692811'):'Bui Vien', 
-    ('21.035127','105.851987'):'Ta Hien',
+    ('21.035127', '105.851987'):'Ta Hien',
     ('16.065015', '108.221882'):'Da Nang',
     ('11.947322', '108.440285'):'Da Lat',
     ('10.782762', '106.696145'):'Ho Con Rua',
@@ -28,7 +18,7 @@ def get_places(lat, lon):
 def autolike():
     recs = tinder_api_sms.get_recommendations()
     if 'results' not in recs:
-        lat, lon = random.choice(list(locations.items()))
+        lat, lon = random.choice(list(places))
         get_places(lat, lon)
         tinder_api_sms.update_location(lat, lon)
         autolike()
